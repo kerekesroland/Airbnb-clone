c02594bb07d1d78db90a9f7c2fbe4e1a9eba2f7c
kerekesroland <kerekes98roland@gmail.com>
2023-04-15 23:10:36 +0200

    Initial setup

---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { Box, Text } from "@chakra-ui/react";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <Box height="50px">
+      <Text fontSize="7xl" fontWeight="400">
+        Navbar
+      </Text>
+    </Box>
+  );
+};
+
+export default Navbar;
