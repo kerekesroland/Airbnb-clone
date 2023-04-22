165a8369d0771fa21013d627252931720db1ef19
kerekesroland <kerekes98roland@gmail.com>
2023-04-23 01:46:04 +0200

    Feat: InputController got a focused prop that lets us customize and animate the placeholders to the top

--- app/components/InputController/InputController.tsx
+++ app/components/InputController/InputController.tsx
@@ -10,6 +10,8 @@ import {
   NumberInputField,
 } from "@chakra-ui/react";
 import { ErrorMessage } from "../ErrorMessage/ErrorMessage";
+import { useState } from "react";
+import { AnimatedInputLabel } from "../InputLabel/InputLabel";
 
 export function InputController({
   label,
@@ -19,46 +21,62 @@ export function InputController({
   register,
   placeholder,
   labelWeight = "bold",
+  value = "",
 }: IInputController) {
+  const [isFocused, setIsFocused] = useState(false);
+
   return (
     <FormControl
       className="form-controller"
       id={placeholder}
       marginBottom={"2rem"}
     >
-      {label ? <FormLabel fontWeight={labelWeight}>{label}</FormLabel> : null}
-      <div className="form-controller">
-        {type === "number" ? (
-          <Box className="number-icon-wrapper" display="flex">
-            <NumberInput
-              variant="withIcon"
-              defaultValue={1}
-              min={1}
-              display="flex"
-            >
-              <NumberInputField {...register} />
-            </NumberInput>
-          </Box>
-        ) : (
-          <Input
-            w="100%"
-            backgroundColor="#FFF"
-            padding="10px 14px"
-            height="initial"
-            border="1px solid #b0b0b0"
-            borderRadius="6px"
-            color="#505050"
-            _placeholder={{
-              color: "#A0AEC0",
-            }}
-            fontSize="lg"
-            placeholder={placeholder}
-            type={type}
-            autoComplete="off"
-            {...register}
-          />
-        )}
-      </div>
+      <Box position="relative" minHeight="40px">
+        {label ? (
+          <AnimatedInputLabel
+            fontWeight={labelWeight}
+            isFocused={isFocused || register.value}
+            value={value}
+          >
+            {label}
+          </AnimatedInputLabel>
+        ) : null}
+        <div className="form-controller">
+          {type === "number" ? (
+            <Box className="number-icon-wrapper" display="flex">
+              <NumberInput
+                variant="withIcon"
+                defaultValue={1}
+                min={1}
+                display="flex"
+              >
+                <NumberInputField {...register} />
+              </NumberInput>
+            </Box>
+          ) : (
+            <Input
+              w="100%"
+              backgroundColor="#FFF"
+              padding="15px 14px"
+              minHeight="40px"
+              border="1px solid #b0b0b0"
+              borderRadius="6px"
+              color="#505050"
+              _placeholder={{
+                color: "#A0AEC0",
+              }}
+              fontSize="lg"
+              placeholder={placeholder}
+              type={type}
+              autoComplete="off"
+              {...register}
+              value={value}
+              onFocus={() => setIsFocused(true)}
+              onBlur={() => setIsFocused(false)}
+            />
+          )}
+        </div>
+      </Box>
       <ErrorMessage isTouched={isTouched} error={error} />
     </FormControl>
   );
