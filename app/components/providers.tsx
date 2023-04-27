5fffcf34be75c8c4e635b654d3c9568b48ff25bd
kerekesroland <kerekes98roland@gmail.com>
2023-04-27 08:35:47 +0200

    Feat: Added ToastProvider

--- app/components/providers.tsx
+++ app/components/providers.tsx
@@ -3,11 +3,12 @@
 
 import { CacheProvider } from "@chakra-ui/next-js";
 import { ChakraProvider } from "@chakra-ui/react";
-
+import ToastProvider from "./providers/ToastProvider";
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <CacheProvider>
       <ChakraProvider>{children}</ChakraProvider>
+      <ToastProvider />
     </CacheProvider>
   );
 }
