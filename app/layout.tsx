bdb9a0c7a54fbd61b7dacddb02774b9d26ec7e9c
kerekesroland <kerekes98roland@gmail.com>
2023-04-29 20:07:17 +0200

    Feat: Created the ClientOnly Wrapper because nextjs 13 hydration error

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Nunito } from "next/font/google";
 import Navbar from "@/app/components/Navbar/Navbar";
 import Footer from "@/app/components/Footer/Footer";
 import RegisterModal from "./components/modals/RegisterModal";
+import ClientOnly from "./components/ClientOnly";
 
 const font = Nunito({
   subsets: ["latin"],
@@ -22,8 +23,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <RegisterModal />
-        <Navbar />
+        <ClientOnly>
+          <RegisterModal />
+          <Navbar />
+        </ClientOnly>
         <Providers>{children}</Providers>
         <Footer />
       </body>
