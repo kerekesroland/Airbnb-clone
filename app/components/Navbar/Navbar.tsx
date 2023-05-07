bd67d65325452c16d42f3f31a17859398c99297b
kerekesroland <kerekes98roland@gmail.com>
2023-05-07 15:17:36 +0200

    Feat: Added Categories field

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -5,22 +5,31 @@ import React from "react";
 import { Logo } from "../Icons/Icons";
 import SearchBar from "./Searchbar";
 import Auth from "./Auth";
+import { IUser } from "@/app/models";
+import Categories from "../Categories/Categories";
 
-const Navbar = () => {
+interface IProps {
+  user: IUser | null;
+}
+
+const Navbar = ({ user }: IProps) => {
   return (
-    <Box className={styles.nav__container}>
-      <Flex className={styles.nav_items__container}>
-        <Box className={styles.logo__container}>
-          <Logo />
-        </Box>
-        <Box className={styles.search__container}>
-          <SearchBar />
-        </Box>
-        <Box display="flex" justifyContent="center" alignItems="center">
-          <Auth />
-        </Box>
-      </Flex>
-    </Box>
+    <>
+      <Box className={styles.nav__container}>
+        <Flex className={styles.nav_items__container}>
+          <Box className={styles.logo__container}>
+            <Logo />
+          </Box>
+          <Box className={styles.search__container}>
+            <SearchBar />
+          </Box>
+          <Box display="flex" justifyContent="center" alignItems="center">
+            <Auth user={user} />
+          </Box>
+        </Flex>
+      </Box>
+      <Categories />
+    </>
   );
 };
 
