8fa45b679b8c2d791b5fd524bb85b2d1e88470a5
kerekesroland <kerekes98roland@gmail.com>
2023-05-07 15:16:30 +0200

    Feat: Added getCurrentUser function to get the currently logged in user

---
+++ app/utils/getCurrentUser.ts
@@ -0,0 +1,34 @@
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { getServerSession } from "next-auth/next";
+import prisma from "@/app/libs/prismadb";
+
+const getSession = async () => {
+  return await getServerSession(authOptions);
+};
+
+const getCurrentUser = async () => {
+  try {
+    const session = await getSession();
+
+    if (!session?.user?.email) return null;
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email: session.user.email,
+      },
+    });
+
+    if (!user) return null;
+
+    return {
+      ...user,
+      createdAt: user.createdAt?.toISOString() as string,
+      updatedAt: user.updatedAt?.toISOString() as string,
+      emailVerified: user.emailVerified?.toISOString() || null,
+    };
+  } catch (error) {
+    return null;
+  }
+};
+
+export default getCurrentUser;
