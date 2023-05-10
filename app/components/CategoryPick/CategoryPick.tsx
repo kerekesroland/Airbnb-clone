2ae3fd532e48efdc412d4dfaa07bb6d5086ee174
kerekesroland <kerekes98roland@gmail.com>
2023-05-10 21:13:19 +0200

    Feat: Added animated categorypick component

---
+++ app/components/CategoryPick/CategoryPick.tsx
@@ -0,0 +1,50 @@
+import { Flex, Text } from "@chakra-ui/react";
+import React from "react";
+import { IconType } from "react-icons";
+import styles from "./CategoryPick.module.scss";
+import { motion } from "framer-motion";
+
+interface IRentInputProps {
+  propertyType: string;
+}
+type Props = {
+  label: string;
+  desc: string;
+  image: IconType;
+  active: string;
+  onChange: (id: keyof IRentInputProps, value: any) => void;
+
+  setActive: React.Dispatch<React.SetStateAction<string>>;
+};
+
+const CategoryPick = ({
+  label,
+  desc,
+  image: Icon,
+  active,
+  setActive,
+  onChange,
+}: Props) => {
+  const classes = (label: string) => {
+    return label === active
+      ? styles.categoryPick.concat(" ").concat(styles.active)
+      : styles.categoryPick;
+  };
+  const handleActivePick = (label: string) => {
+    setActive(label);
+  };
+  return (
+    <Flex
+      onClick={() => {
+        handleActivePick(label);
+        onChange("propertyType", label);
+      }}
+      className={classes(label)}
+    >
+      <Icon size={28} />
+      <Text>{label}</Text>
+    </Flex>
+  );
+};
+
+export default CategoryPick;
