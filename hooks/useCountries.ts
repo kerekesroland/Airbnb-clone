b5e667fae86670354507f5a7e5dce306334ccf43
kerekesroland <kerekes98roland@gmail.com>
2023-05-10 21:11:52 +0200

    Feat: Added useCountries to the hooks folder

---
+++ hooks/useCountries.ts
@@ -0,0 +1,25 @@
+import countries from "world-countries";
+
+const formattedCountries = () => {
+  return countries.map((country) => {
+    return {
+      value: country.cca2,
+      label: country.name.common,
+      flag: country.flag,
+      latlong: country.latlng,
+      region: country.region,
+    };
+  });
+};
+
+const useCountries = () => {
+  const getCountries = () => formattedCountries();
+
+  const getCountry = (value: string) =>
+    formattedCountries().find((country) => country.value === value);
+
+  return {
+    getCountries,
+    getCountry,
+  };
+};
