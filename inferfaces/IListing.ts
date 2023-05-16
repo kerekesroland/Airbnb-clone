fd943d77972f34a07aa580fca2a613bf6c5dabff
kerekesroland <kerekes98roland@gmail.com>
2023-05-16 14:48:18 +0200

    Chore: Added new interface IListing

---
+++ inferfaces/IListing.ts
@@ -0,0 +1,15 @@
+export interface IListing {
+  id: string;
+  userId: string;
+  title: string;
+  description: string;
+  price: number;
+  image: string;
+  category: string;
+  numberOfRooms: number;
+  numberOfBathrooms: number;
+  numberOfGuests: number;
+  locationValue: string;
+  createdAt: string;
+  updatedAt: string;
+}
