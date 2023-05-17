dbc8e02d2585d33e5d2f30dd0fdf8b67c4f8f10e
kerekesroland <kerekes98roland@gmail.com>
2023-05-17 17:36:48 +0200

    Feat: Added Ilisting

--- app/models/index.ts
+++ app/models/index.ts
@@ -1,7 +1,12 @@
-import { User } from "@prisma/client";
+import { Listing, User } from "@prisma/client";
 
 export type IUser = Omit<User, "createdAt" | "updatedAt" | "emailVerified"> & {
   createdAt: string;
   updatedAt: string;
   emailVerified: string | null;
+};
+
+export type IListing = Omit<Listing, "createdAt" | "updatedAt"> & {
+  createdAt: string;
+  updatedAt: string;
 };
