5b3c029ce21c5957090c8e0735aa0424e5834018
kerekesroland <kerekes98roland@gmail.com>
2023-05-22 16:06:02 +0200

    Chore: Added getReservations

--- app/listings/[id]/page.tsx
+++ app/listings/[id]/page.tsx
@@ -1,18 +1,45 @@
 import ClientOnly from "@/app/components/ClientOnly";
 import ListingDetails from "@/app/components/ListingDetails/ListingDetails";
+import { getAllListings } from "@/app/queries/getAllListings";
 import { getListing } from "@/app/queries/getListing";
+import { getReservations } from "@/app/queries/getReservations";
 import getCurrentUser from "@/app/utils/getCurrentUser";
 import React from "react";
 
-const ListingPage = async ({ params }: { params: { id: string } }) => {
+interface IParams {
+  id: string;
+  userId: string;
+  authorId: string;
+}
+
+// export const revalidate = 300;
+
+// export const generateStaticParams = async () => {
+//   const listings = await getAllListings();
+//   return listings?.map((listing) => {
+//     return {
+//       id: listing?.id,
+//     };
+//   });
+// };
+
+const ListingPage = async ({ params }: { params: IParams }) => {
   const listing = await getListing(params.id);
+  const reservations = await getReservations(params);
   const user = await getCurrentUser();
+
+  // console.log(user);
+
   if (!listing || !user) {
     return <></>;
   }
   return (
     <ClientOnly>
-      <ListingDetails user={user} listing={listing} />
+      <ListingDetails
+        user={user}
+        listing={listing}
+        reservations={reservations}
+      />
     </ClientOnly>
   );
 };
